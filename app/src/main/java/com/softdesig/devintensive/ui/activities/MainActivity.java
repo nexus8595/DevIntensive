add3c6178f767856d050d01de8c882ee966518f1
nexus85 <mx38@mail.ru>
2016-06-20 01:05:17 +0300

    Initial commit

---
+++ app/src/main/java/com/softdesig/devintensive/ui/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.softdesig.devintensive.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.softdesig.devintensive.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
